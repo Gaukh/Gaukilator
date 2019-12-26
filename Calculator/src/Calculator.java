b401f5ba30f7f80bba8c557c3064e822787f280d
Gaukh <derennetel@yahoo.de>
2019-12-26 14:01:10 +0100

    Import of local Calculator.java and Numlet.java

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -2,55 +2,100 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
+
+
     public static void main(String[] args) {
+
         SimpleCalculation();
     }
 
     public static void SimpleCalculation() {
 
-        double input1;
+        String inputChar;
+        char n;
+        char l;
+        double input1num;
+        String input1let;
         char operator;
-        double input2;
+        double input2num;
 
         Scanner myCalc = new Scanner(System.in);
+        Scanner myLet = new Scanner(System.in);
         Scanner myOp = new Scanner(System.in);
 
-        try {
-            System.out.println("Enter your first digit(s) or operating letter:");
-            input1 = myCalc.nextInt();
+        System.out.println("What do you want to calculate with? Numbers or letters? Type l for letter and n for number");
+        inputChar = myLet.next();
 
-            System.out.println("Enter +, -, *, or / below:");
-            operator = myOp.next().charAt(0);
+        switch (inputChar) {
 
-            System.out.println("Enter your second digit(s):");
-            input2 = myCalc.nextDouble();
+            case "n": {
 
-            switch (operator) {
-                case '+':
-                    if (operator == '+') {
-                        System.out.println(input1 + input2);
-                    }
-                    break;
-                case '-':
-                    if (operator == '-') {
-                        System.out.println(input1 - input2);
-                    }
-                    break;
-                case '*':
-                    if (operator == '*') {
-                        System.out.println(input1 * input2);
-                    }
-                    break;
-                case '/':
-                    if (operator == '/' && input2 == 0) {
-                        System.out.println("You can't divide by zero, idiot.");
-                    } else if (operator == '/' && input2 != 0) {
-                        System.out.println(input1 / input2);
-                    }
-                    break;
-                default:
-                    break;
+                System.out.println("Enter your first digit(s):");
+                input1num = myCalc.nextInt();
+
+                System.out.println("Enter +, -, *, or / below:");
+                operator = myOp.next().charAt(0);
+
+                System.out.println("Enter your second digit(s):");
+                input2num = myCalc.nextDouble();
+
+                switch (operator) {
+                    case '+':
+                        if (operator == '+') {
+                            System.out.println(input1num + input2num);
+                        }
+                        break;
+                    case '-':
+                        if (operator == '-') {
+                            System.out.println(input1num - input2num);
+                        }
+                        break;
+                    case '*':
+                        if (operator == '*') {
+                            System.out.println(input1num * input2num);
+                        }
+                        break;
+                    case '/':
+                        if (operator == '/' && input2num == 0) {
+                            System.out.println("You can't divide by zero, idiot.");
+                        } else if (operator == '/' && input2num != 0) {
+                            System.out.println(input1num / input2num);
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid operator.");
+                        break;
+                }
+                break;
             }
+
+            case "l": {
+                System.out.println("Enter your letter you want to calculate with:");
+                input1let = myLet.next();
+
+                switch (input1let) {
+                    case "f":
+                        int i;
+                        int result;
+                        for (i = 3; i > 0; i--) {
+                            result = i * i - 1;
+                            System.out.println(result);
+                    }
+
+                }
+
+                System.out.println("Enter your digit(s):");
+                input2num = myCalc.nextDouble();
+                break;
+            }
+
+            default:
+                break;
+
+        }
+
+
+        try {
 
         } catch (InputMismatchException ime) {
             System.out.println("No letters allowed, this is a simple calculator you fuckyducky."); }
--- Calculator/src/Numlet.java
+++ Calculator/src/Numlet.java
@@ -1,6 +1,11 @@
 public class Numlet extends Calculator {
-    char facul = 'f';
-    int[] myNum1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-    int[] myNum2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    private char facul = 'f';
+    private int[] input1num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    private int[] input2num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+    public String setInput1num(String input1num) {
+
+        return input1num;
+    }
 
 }
